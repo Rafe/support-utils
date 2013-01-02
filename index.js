97763e17c0f64080e3b19fa392b829d5aa6a1fa7
Jimmy Chao <daizenga@gmail.com>
2013-01-02 15:32:12 -0500

    add object

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('coffee-script');
 
-exports.password = require('./lib/password')
-exports.string = require('./lib/string')
-exports.form_helper = require('./lib/form_helper')
+exports.password = require('./lib/password');
+exports.string = require('./lib/string');
+exports.form_helper = require('./lib/form_helper');
+exports.object = require('./lib/object');
